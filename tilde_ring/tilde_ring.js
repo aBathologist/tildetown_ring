bf4e6760c3d2edd7a207bf2dceb37dad8895d81f
aBathologist <aBathologist@gmail.com>
2014-12-10 09:19:10 +0000

    Changes:
    - refactored js (mainly so that user.json would only be called in once)
    - reorganized js
    - added comments to break up sections of js
    - added conditional checks for the "random ~box" link, so this link can no be
      omitted without breaking anything
    - updated join.html to include "next ~user" link

--- tilde_ring/tilde_ring.js
+++ tilde_ring/tilde_ring.js
@@ -1,33 +1,116 @@
 // This code is totally beta
 //
-//  written by ~um@tilde.town (@shon_feder)
-//  with lots of help form ~dan, ~karlen, ~datagrok, ~nick, ~vilmibm
+//  written by ~um@tilde.town (@shon_feder) and ~dan@tilde.town
+//  with lots of help form ~karlen, ~datagrok, ~nick, ~vilmibm
 //  and tilde.town in general.
+
+/*
+ * CONSTANTS
+ *
+ * */
 
 var tildeboxlist_urls = 'http://tilde.town/~um/json/othertildes.json';
 var userlist_url = 'http://tilde.town/~dan/users.json';
 
-// main functions, run when script loads.
+/*
+ *  MAIN FUNCTIONS
+ * 
+ * */
+
+// run when page is loaded
 window.onload = function() {
     add_event_listeners();
     link_tilde_ring();
 };
 
-function link_tilde_ring() {
-    link_random_tildebox();
-    link_random_user();
-    link_next_user();
-}
-
 function add_event_listeners() {
-    var ring_link      = document.getElementById('tilde_town_ring'),
+    var ring_link_rand = document.getElementById('tilde_town_ring'),
         rand_box_link  = document.getElementById('random_tildebox'),
         ring_link_next = document.getElementById('tilde_town_ring_next');
 
-    ring_link.addEventListener('click', link_tilde_ring);
-    rand_box_link.addEventListener('click', link_tilde_ring);
-    if (ring_link_next) {
-        ring_link_next.addEventListener('click', link_tilde_ring);
+    ring_link_rand.addEventListener('click', link_tilde_ring);
+    
+    add_tilde_ring_listener_if_present( rand_box_link );
+    add_tilde_ring_listener_if_present( ring_link_next );
+}
+
+function link_tilde_ring() {
+    if (document.getElementById('random_tildebox')) {link_random_tildebox();};
+    link_users();
+}
+
+
+/*
+ * FETCHING DATA AND GENERATING HREF VALUES FOR LINKS
+ *
+ * */
+
+// generates a random href value for a link dependng on the list of tilde boxes
+function link_random_tildebox() {
+    fetchJSONFile( tildeboxlist_urls ,
+                   function(data) {
+                       var urls = normalize_tilde_urls( obj_values(data) );
+                       document.getElementById('random_tildebox').href = random_item(urls);
+                   });
+}
+
+// generates href values for links that depend upon the user list
+function link_users() {
+    fetchJSONFile( userlist_url,
+                   function(users) {
+
+                       var user_url = users[user()].homepage,
+                           urls     = normalized_user_urls(obj_values(users)),
+                           
+                           random_user_link = document.getElementById('tilde_town_ring'),
+                           next_user_link   = document.getElementById('tilde_town_ring_next');
+
+                       random_user_link.href = random_item(remove(urls, user_url));
+                       if (next_user_link) { // ensure user has next link
+                           next_user_link.href = next_item(user_url, urls);
+                       }
+
+                   });
+}
+
+
+/*
+ * NORMALIZING DATA
+ *
+ * */
+
+// normalized tilde urls have trailing slashes trimmed
+// remove the host from the array
+function normalize_tilde_urls(urls) {
+    urls.push('http://tilde.club');     // compensating for tilde.club's club-centricism ;)
+
+    var trimmed_urls  = urls.map(trim_trailing_slash),
+        host_tildebox = window.location.hostname;
+
+    return remove(trimmed_urls, host_tildebox);
+}
+
+// normalized_user_urls :: [users] -> [user_urls]
+// "normalization" here means ...
+// - eliminating users who haven't joined the ring
+// - sorting the list
+// - and returning the homepage urls of the remainder.
+function normalized_user_urls(users) {
+    return users.filter(function(u) {return u.ringmember;})
+                .map(function(o) { return o.homepage; })
+                .sort();
+}
+
+/*
+ * GET PAGE-ENVIORONMENT VALUES
+ *
+ * */
+
+// use to add listeners for links which are option to
+// include: e.g., "random ~box" and "next ~user"
+function add_tilde_ring_listener_if_present(link) {
+    if (link) {
+        link.addEventListener('click', link_tilde_ring);
     }
 }
 
@@ -35,6 +118,11 @@ function add_event_listeners() {
 function user() {
     return window.location.pathname.split('/')[1].split('~')[1];
 }
+
+/*
+ * GENERAL UTILITIES
+ *
+ * */
 
 // this vanilla.js ajax approach copied from user dystroy's
 // SO answer: http://stackoverflow.com/a/14388512/1187277
@@ -52,108 +140,6 @@ function fetchJSONFile(path, callback) {
     httpRequest.send(); 
 }
 
-// TODO: serve othertildes.json in JSONP so that this scripts can just be
-//       dropped into any page.
-function link_random_tildebox() {
-    fetchJSONFile( tildeboxlist_urls ,
-                   function(data) {
-                       var urls = normalize_tilde_urls( obj_values(data) );
-                       document.getElementById('random_tildebox').href = random_item(urls);
-                   });
-}
-
-// TODO: get tildes to store user JSON in standard location?
-function link_random_user() {
-    fetchJSONFile( userlist_url,
-                   function(users) {
-                       delete users[user()];
-                       var urls   = normalize_for_random_user(obj_values(users)),
-                           tilde_ring_link = document.getElementById('tilde_town_ring');
-
-                       // console.log("other ~ring members: ");
-                       // console.log(urls);
-
-                       tilde_ring_link.href = random_item(urls);
-                   });
-}
-
-function link_next_user() {
-    fetchJSONFile( userlist_url,
-                   function(users) {
-                       var urls = normalize_for_next_user(obj_values(users)),
-                           tilde_ring_link = document.getElementById('tilde_town_ring_next');
-                       if (tilde_ring_link) { // ensure user has next link
-                           tilde_ring_link.href = next_item(urls);
-                       }
-                   });
-}
-
-// "normalizing" tilde urls means removing traling slashes and
-// removing the host from the array
-function normalize_tilde_urls(urls) {
-    urls.push('http://tilde.club');     // compensating for tilde.club's club-centricism ;)
-
-    function last(array) {
-        return array[array.length - 1];
-    }
-
-    function trim_trailing_slash(string) {
-        return ( (last(string) == "/")
-                 ? string.slice(0, string.length-1)
-                 : string
-               );
-    }
-
-    var trimmed_urls  = urls.map(trim_trailing_slash),
-        host_tildebox = window.location.hostname;
-
-    return remove(trimmed_urls, host_tildebox);
-}
-
-// "normalizing" for a random user means eliminating users who haven't
-// edited their pages and haven't joined the ring
-// and returning the remainder.
-function normalize_for_random_user(users) {
-    return users.filter(function(u) {return u.edited && u.ringmember;})
-                .map(function(o) { return o.homepage; });
-
-    // ring_members = user_props.filter(function(x){ return ; }),
-    // member_urls  = ring_members.,
-
-}
-
-function normalize_for_next_user(users) {
-    sorted_users = users.filter(function(u) {return u.edited && u.ringmember;})
-                .map(function(o) { return o.homepage; });
-    return sorted_users.sort()
-}
-
-
-function remove(array, x) {
-    var index = array.indexOf(x);
-    if ( index > -1 ) {
-        array.splice(index, 1);
-    };
-    return array;
-}
-
-
-function random_item(items) {
-    return items[Math.floor(Math.random()*items.length)];
-}
-
-function next_item(items) {
-    var member_url = strip_trailing_slash(document.URL);
-    for (var i = 0; i < items.length; i++) {
-        if (member_url == items[i]) {
-             var index = i + 1;
-             break;
-        }
-    }
-    if (index > items.length) { index = 0; }
-    return items[index];
-}
-
 // Object {key:values} -> Array [values]
 function obj_values(obj) {
     var values = [];
@@ -165,9 +151,37 @@ function obj_values(obj) {
     return values;
 }
 
-function strip_trailing_slash(str) {
-    if(str.substr(-1) == '/') {
-        return str.substr(0, str.length - 1);
-    }
-    return str;
+function last(array) {
+    return array[array.length - 1];
+}
+
+function trim_trailing_slash(string) {
+    return ( (last(string) == "/")
+             ? string.slice(0, string.length-1)
+             : string
+           );
+}
+
+function remove(array, x) {
+    var index = array.indexOf(x);
+    if ( index > -1 ) {
+        array.splice(index, 1);
+    };
+    return array;
+}
+
+function random_item(items) {
+    return items[Math.floor(Math.random()*items.length)];
+}
+
+// next_item :: (ref_item, [items]) -> next_item
+//
+function next_item(item, items) {
+    var item_index = items.indexOf(item),
+        next_index = item_index == (items.length - 1)
+                     ? 0
+                     : item_index + 1
+                     ;
+                    
+    return items[next_index];
 }
